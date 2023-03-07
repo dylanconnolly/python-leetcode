3e60acb3677499642ce7cc6880ea0896955b869e
Dylan Connolly <dylanconnolly145@gmail.com>
2023-03-07 15:56:13 -0700

    group anagrams

---
+++ hash-maps/group_anagrams.py
@@ -0,0 +1,23 @@
+from typing import List
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hash = {}
+        
+        for string in strs:
+            alphabetized = ''.join(sorted(string))
+            if alphabetized in hash:
+                hash[alphabetized].append(string)
+            else:
+                hash[alphabetized] = [string]
+                
+        result = []
+        
+        for v in hash.values():
+            result.append(v)
+            
+        return result
+
+
+r = Solution().groupAnagrams(["eat","tea","tan","ate","nat","bat"])
+print(r)
