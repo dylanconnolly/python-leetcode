0dbd655a2b8b1e67b7ca319e26d953625d2a0f9c
Dylan Connolly <dylanconnolly145@gmail.com>
2023-03-18 22:26:32 -0600

    buy and sell stock

---
+++ two-pointers/trapping_rain_water.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        
+
+def test(arr):
+    print(Solution().trap(arr))
+
+test([0,1,0,2,1,0,1,3,2,1,2,1]) # should equal 6
