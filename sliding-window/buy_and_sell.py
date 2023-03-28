79a4826984863a1201dc95d6d4714e16ade6bce2
Dylan Connolly <dylanconnolly145@gmail.com>
2023-03-27 22:27:51 -0600

    change var names

--- sliding-window/buy_and_sell.py
+++ sliding-window/buy_and_sell.py
@@ -1,10 +1,20 @@
+from typing import List
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        res = 0
+        maxProfit = 0
         
         lowest = prices[0]
+
         for price in prices:
             if price < lowest:
                 lowest = price
-            res = max(res, price - lowest)
-        return res
+            maxProfit = max(maxProfit, price - lowest)
+        return maxProfit
+
+
+def test(prices):
+    print(Solution().maxProfit(prices))
+
+
+test([7,1,5,3,6,4])
